8544331ef18d8c2961e001a433d7fe66b611e026
MoralesMortales <moralesyaguilera@gmail.com>
2025-06-05 19:54:12 -0400

    done

--- src/components/navbar/navbar.vue
+++ src/components/navbar/navbar.vue
@@ -1,17 +1,65 @@
 <template>
-  <div class="w-full bg-white h-16" id="navbar">
-    <div id="logo">
-      <img src="@/assets/img/logoKitchen.png" alt="">
+  <div class="w-full bg-white flex justify-between" style="height: 9dvh;" id="navbar">
+    <div id="logo" class="h-full p-2">
+      <img src="@/assets/img/logoKitchen.png" alt="" class="h-full">
     </div>
-hallo
+
+    <div id="right" class="h-full flex justify-center items-center">
+      <div class="h-full flex justify-center">
+        <q-toolbar>
+          <q-btn flat dense round icon="menu" size="22px" aria-label="Menu" @click="drawer = !drawer" />
+        </q-toolbar>
+      </div>
+    </div>
+    </div>
+
+    <div class="relative">
+      <q-layout view="hHh Lpr lFf" style="height: 91dvh;" container class="absolute shadow-2 rounded-borders">
+    <q-drawer 
+      v-model="drawer"
+      side="right"
+      :width="300"
+      :breakpoint="700"
+      bordered
+      behavior="mobile"
+      overlay
+      :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-3'"
+    >
+          <q-scroll-area class="fit">
+            
+            <q-list>
+
+              <template v-for="(menuItem, index) in menuList" :key="index">
+                <q-item clickable :active="menuItem.label === 'Outbox'" v-ripple>
+                  <q-item-section>
+                    {{ menuItem.label }}
+                  </q-item-section>
+                </q-item>
+                <q-separator :key="'sep' + index" v-if="menuItem.separator" />
+              </template>
+
+            </q-list>
+
+          </q-scroll-area>
+        </q-drawer>
+      </q-layout>
 
   </div>
 </template>
 
 <script setup>
 
+import { ref } from 'vue'
+
+let drawer = ref(false);
+
+const menuList = ref([
+  {
+    label: 'Menu',
+    separator: true
+  }
+])
+
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { createApp } from 'vue'
 import './style.css'
 import './assets/main.css'
+
+import { Quasar } from 'quasar'
+
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(Quasar, {
+  plugins: {}, // import Quasar plugins here
+})
+
+app.mount('#app')
