d86a238f38460385b7377f9471b3a5a8654d5a92
MoralesMortales <carlos52.7624@gmail.com>
2025-06-08 12:14:04 -0500

    details

---
+++ src/components/listFood/RecipeDetails.vue
@@ -0,0 +1,34 @@
+
+<template>
+  <div class="">
+    holaa
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted, computed } from "vue";
+import { useRoute } from "vue-router";
+import { getRecipeById } from "../../axios/axios";
+
+const route = useRoute();
+const isLoading = ref(false);
+const error = ref(null);
+
+const props = defineProps({
+ id: {
+    type: Number,
+    required: true,
+  }
+});
+
+console.log(getRecipeById(props.id));
+
+</script>
+
+<style lang="scss" scoped>
+.recipeTitle {
+  font-family: "Abel", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+}
+</style>
